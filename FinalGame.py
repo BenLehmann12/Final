19dbce77d5cf2a1d589e9b7a8fdfd78507a5b3aa
Ben Lehmann <62218426+BenLehmann12@users.noreply.github.com>
2020-12-14 17:16:12 -0600

    Update FinalGame.Py

---
+++ FinalGame.py
@@ -0,0 +1,56 @@
+import tkinter as tk
+import random
+
+class Game:
+    def __init__(self,Master):
+        new_frame = tk.Frame(Master)
+        new_frame.pack()
+        self.winner = tk.Label(window, text="Start the Game")
+        self.winner.pack()
+
+        self.options = [('rock', 0), ('paper', 1), ('scissors', 2)]  #Create a list, with tuples, each with a score, rock is 0, paper is 1, scissors is 2
+        self.rock = tk.Button(new_frame, text='Rock', command=lambda: self.test(self.options[0]))
+        self.rock.grid(row=1, column=1)
+
+        self.paper = tk.Button(new_frame, text='Paper', command=lambda: self.test(self.options[1]))
+        self.paper.grid(row=1, column=2)
+
+        self.scissors = tk.Button(new_frame, text='Scissors', command=lambda: self.test(self.options[2]))
+        self.scissors.grid(row=1, column=3)
+
+        self.user_display = tk.Label(new_frame, text="You Selected ")
+        self.user_display.grid(row=3, column=1)
+
+        self.computer_display = tk.Label(new_frame, text="The Computer Selects: ")
+        self.computer_display.grid(row=4, column=1)
+
+    def computer_selection(self):
+        self.computer_choice = random.choice(self.options)
+        try:
+            return self.computer_choice
+        except:
+            return "Error"
+
+    def test(self,input):
+        computer_select = self.computer_selection()
+        self.user_display.config(text="You Chose: " + input[0])
+        self.computer_display.config(text="The Computer Chose: " + computer_select[0])
+        if (input == computer_select):
+            self.winner.config(text="It's a Tie")
+            print("Tie")
+        elif ((input[1] - computer_select[1]) % 3 == 1):
+            print("You Won")
+            self.winner.config(text="You are the winner")
+        else:
+            print("The Computer Won")
+            self.winner.config(text="The Computer is the Winner")
+
+
+
+
+
+
+
+
+
+
